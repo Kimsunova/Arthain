587fc0dac3ca5de28b7e57aa20601267115ae03a
Ripper Smith <rippersmith61@gmail.com>
2019-11-18 20:48:21 -0500

    Updated to Unity 2019
    updated the project to Unity 2019.2.12f1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public float health, stamina;
+    public int currency;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
