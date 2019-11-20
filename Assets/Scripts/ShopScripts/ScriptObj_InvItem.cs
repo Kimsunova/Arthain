4ce712a8d79ab77d1b6e6864f8ed00faed328237
Ripper Smith <rippersmith61@gmail.com>
2019-11-20 12:20:50 -0500

    Inventory/Shop Start
    Started making scripts & imported assets to test inventory & shop system

---
+++ Assets/Scripts/ShopScripts/ScriptObj_InvItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Inventory Item")]
+public class ScriptObj_InvItem : ScriptableObject
+{
+    public enum Tag { Weapon, Heal, Buff, PermBuff }; //Tag denotes the type of object (buff, heal, etc). Add more types as needed.
+    public Tag itemTag;
+    public string itemName; //the name of the item, for display and such
+    public Sprite itemImage; //an image of the item to display on the inventory menu
+    public string description; //the description of the object for the shop or if player looks at the item
+
+    public int itemPrice;
+    public int itemSell;
+    public float incrementValue; //most items will be healing items or buffs of some sort
+
+}
---
+++ Assets/Scripts/ShopScripts/ScriptObj_Shop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Shop Item")]
+public class ScriptObj_Shop : ScriptableObject
+{
+    public ScriptObj_InvItem invItem; //The item to spawn
+    public enum Tag { Weapon, Heal, Buff, PermBuff }; //InventoryType denotes the type of object (pet, hat, etc). The inventory menu sorts items by type. Add more types as needed.
+
+    public Tag itemType;
+    public string itemName; //the name of the item, for display and such
+    public string description; //description of the object
+    public Sprite itemImage; //an image of the item to display on the inventory menu
+    public int price;
+
+    private void Awake()
+    {
+        itemType = (Tag) invItem.itemTag;
+        itemName = invItem.itemName;
+        description = invItem.description;
+        itemImage = invItem.itemImage;
+        price = invItem.itemPrice;
+    }
+}
+
+
