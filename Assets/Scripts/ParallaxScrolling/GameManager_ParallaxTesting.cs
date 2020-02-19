ffb8406ced90b0e210d5230e00305e64eea06e15
Ripper Smith <rippersmith61@gmail.com>
2020-02-19 16:29:57 -0500

    Parallax/ArrangedFolders/VSync
    -made the parallax scrolling working in the "ParallaxTesting" scene
    -disabled VSync to preserve processing power in the future
    -rearranged some of the folders, mainly in the Inventory folder

---
+++ Assets/Scripts/ParallaxScrolling/GameManager_ParallaxTesting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_ParallaxTesting : MonoBehaviour
+{
+    public Transform[] backgrounds;     //arrays of all backgrounds/foregrounds to be parallaxed
+    public float[] parallaxScales;     //proportion of camera's movement for moving backgrounds
+    public float smoothing = 1f;        //how smooth parallax will be
+
+    private Transform cam;
+    private Vector3 previousCameraPosition;
+    private int backgroundsLength;
+
+    private float parallax, backgroundTargetPosX;
+
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+        previousCameraPosition = cam.position;
+        backgroundsLength = backgrounds.Length;
+        parallaxScales = new float[backgrounds.Length];
+
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = -backgrounds[i].position.z;
+        }
+
+    }
+
+    void LateUpdate()
+    {
+        for (int i = 0; i < backgroundsLength; i++)
+        {
+            parallax = (previousCameraPosition.x - cam.position.x) * parallaxScales[i];            
+            backgroundTargetPosX = backgrounds[i].position.x + parallax;
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+
+        previousCameraPosition = cam.position;
+
+    }
+}
